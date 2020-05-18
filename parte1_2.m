261807b640bbbb6533c5e866757237ccde516294
Gustavo H <gustavo.hurtado@usach.cl>
2020-05-17 20:21:13 -0400

    Pequeños cambios

--- parte1_2.m
+++ parte1_2.m
@@ -1,8 +1,14 @@
 format long
 clear all
 
+%Dominio de la función
 x = -10:0.05:10;
-y = 6*exp(x + 18);
 
-graph_log(x,y)
-graph_normal(x,y)
+%Función a graficar c(x)= 6*e^(x+18)
+c = 6*exp(x + 18);
+
+%Gráfico de la función c(x) con base normal
+graph_normal(x,c)
+
+%Gráfico de la función c(x) con base logarítmica
+graph_log(x,c)
