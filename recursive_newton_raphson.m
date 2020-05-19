be08d5d99fd169960fd495db13f24e934c519fac
Gustavo H <gustavo.hurtado@usach.cl>
2020-05-19 00:23:50 -0400

    fix newton raphson

--- recursive_newton_raphson.m
+++ recursive_newton_raphson.m
@@ -3,11 +3,14 @@
 %Entradas: Polinomio de una variable, cantidad máxima de iteraciones
 %tolerancia, valor inicial.
 %Salida: Raíz del polinomio de una variable.
-%Ejemplo de uso:  recursive_newton_raphson([3 2 -2],100,10^(-7),0.5)
+%Ejemplos de uso:  
+%recursive_newton_raphson([3 2 -2],100,10^(-7),0.5) = 0.5486
+%recursive_newton_raphson([1 0 2 -2],100,10^(-7),1) = 0.7709
+%recursive_newton_raphson([2 -2],10,10^(-7),-10) = 1
 
 function [result] = recursive_newton_raphson(fx,max_iter,error,x0)
     fx_value = polyval(fx,x0);
-    dfx = diff(fx);
+    dfx = polyder(fx);
     dfx_value = polyval(dfx,x0);
 
     %Criterio de parada
